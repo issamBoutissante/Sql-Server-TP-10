48a30bae1ee6addf85db8194a4034d87bdcf1ff7
Boutissatne Issam <boutissante.issam.dev@gmail.com>
2021-04-04 14:31:34 +0200

    dstill working ion

--- GestVol base de donne.sql
+++ GestVol base de donne.sql
@@ -1,6 +1,8 @@
 create database GestVol
 use GestVol
-create table pilote(
+drop database GestVol
+use master
+create table Pilote(
 numPil int,
 nomPil varchar(15) not null,
 prenomPil varchar(15) not null,
@@ -9,9 +11,23 @@ salarie money not null,
 prime money not null,
 constraint pk_pilote primary key(numPil)
 )
-create table avoin(
+create table Avoin (
 numAv int,
-nomAv varchar(10),
-capacite int,
-localisation varchar(25)
+nomAv varchar(10) not null,
+capacite int not null,
+localisation varchar(25) not null,
+constraint pk_avion primary key(numAv)
+)
+create table vol(
+numVol int,
+numPil int not null,
+numAv int not null,
+date_Vol datetime not null,
+heure_dep decimal(5,2) not null,
+heure_arr decimal(5,2) not null,
+ville_dep varchar(25) not null,
+ville_arr varchar(25) not null,
+constraint pk_vol primary key(numVol),
+constraint fk_vol_avion foreign key(numAv) references Avion(numAv),
+constraint fk_vol_pilote foreign key(numPil) references Pilote(numPil)
 )
